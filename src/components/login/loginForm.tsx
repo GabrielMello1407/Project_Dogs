4949af92207d5dd7e794731b600c0e05cff1148f
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2024-04-24 13:41:23 -0300

    feat: added new useForm validation
    Added news validations to useForm in login page

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -1,12 +1,37 @@
-import login from '@/actions/login';
+'use client';
 
-export default async function LoginForm() {
+import login from '@/actions/login';
+import { useFormState, useFormStatus } from 'react-dom';
+import Button from '@/components/forms/button';
+
+function FormButton() {
+  const { pending } = useFormStatus();
+
   return (
     <>
-      <form action={login}>
-        <input type="text" name="username" placeholder="Usuário" />
-        <input type="password" name="password" placeholder="Senha" />
-        <button>Entrar</button>
+      {pending ? (
+        <Button disabled={pending}>Enviando...</Button>
+      ) : (
+        <Button>Entrar</Button>
+      )}
+    </>
+  );
+}
+
+export default function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: '',
+    data: null,
+  });
+
+  return (
+    <>
+      <form action={action}>
+        <input type="text" name="username" placeholder="usuário" />
+        <input type="password" name="password" placeholder="senha" />
+        <FormButton />
+        <p>{state.error}</p>
       </form>
     </>
   );
