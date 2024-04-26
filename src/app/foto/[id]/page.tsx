402e241114bb7dd2831c24716bdfacd535a115c2
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2024-04-26 20:49:00 -0300

    feat: added photo content and photo delete

--- src/app/foto/[id]/page.tsx
+++ src/app/foto/[id]/page.tsx
@@ -1,11 +1,34 @@
+import photoGet from '@/actions/photo-get';
+import PhotoContent from '@/components/photo/photo-content';
+import { notFound } from 'next/navigation';
+
+type FotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: FotoIdPage) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return { title: 'Fotos' };
+  return {
+    title: data.photo.title,
+  };
+}
+
 export default async function FotoIdPage({
   params,
 }: {
-  params: { id: number };
+  params: { id: string };
 }) {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return notFound;
   return (
-    <main>
-      <h1>Foto id: {params.id}</h1>
-    </main>
+    <section className="container mainContainer">
+      ,
+      <PhotoContent data={data} single={true} />
+    </section>
   );
 }
