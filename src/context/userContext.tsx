8c9dcd33d1219f3cf1590d9355c025dde5075817
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2024-04-27 13:59:50 -0300

    feat: added validation token

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Logout from '@/actions/logout';
+import validateToken from '@/actions/validate-token';
 import React from 'react';
 
 type User = {
@@ -32,6 +34,14 @@ export function UserContextProvider({
   user: User | null;
 }) {
   const [userState, setUser] = React.useState<User | null>(user);
+
+  React.useEffect(() => {
+    async function validate() {
+      const { ok } = await validateToken();
+      if (!ok) await Logout();
+    }
+    if (userState) validate();
+  }, [userState]);
 
   return (
     <UserContext.Provider value={{ user: userState, setUser }}>
